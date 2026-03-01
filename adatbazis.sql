6ef77797cc78ad37d13cffd88c7bbc42885a85b0
kecske5000 <marton.kristof@students.jedlik.eu>
2026-03-01 19:48:44 +0100

    Database full fix

--- adatbazis.sql
+++ adatbazis.sql
@@ -1,4 +1,4 @@
--- Database
+﻿-- Database
 CREATE DATABASE IF NOT EXISTS car_rental
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_hungarian_ci;
@@ -44,12 +44,13 @@ CREATE TABLE cars (
     id INT PRIMARY KEY,
     brand VARCHAR(50) NOT NULL,
     model VARCHAR(50) NOT NULL,
-    branch_id INT NOT NULL,
     year YEAR NOT NULL,
+    number_of_seats SMALLINT NOT NULL,
+    price INT NOT NULL,
+    license_plate VARCHAR(20) NOT NULL UNIQUE,
+    branch_id INT NOT NULL,
     transmission_id INT NOT NULL,
     fuel_type_id INT NOT NULL,
-    number_of_seats TINYINT NOT NULL,
-    price INT NOT NULL,
     FOREIGN KEY (branch_id) REFERENCES branches(id),
     FOREIGN KEY (transmission_id) REFERENCES transmission_types(id),
     FOREIGN KEY (fuel_type_id) REFERENCES fuel_types(id)
@@ -79,6 +80,7 @@ CREATE TABLE rentals (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+-- Types
 INSERT INTO transmission_types (name) VALUES
 ('automata'),
 ('manuális');
@@ -93,6 +95,7 @@ INSERT INTO air_conditioning_types (name) VALUES
 ('automata'),
 ('manuális');
 
+-- Branches
 INSERT INTO branches VALUES
 (1,'Budapest','1138 Budapest, Váci út 123.','budapest@autoker.hu','+36 1 555 1234'),
 (2,'Győr','9027 Győr, Szent István út 45.','gyor@autoker.hu','+36 96 555 678'),
@@ -103,6 +106,7 @@ INSERT INTO branches VALUES
 (7,'Miskolc','3525 Miskolc, Széchenyi utca 88.','miskolc@autoker.hu','+36 46 555 444'),
 (8,'Zalaegerszeg','8900 Zalaegerszeg, Kossuth Lajos utca 21.','zalaegerszeg@autoker.hu','+36 92 555 555');
 
+-- Users
 INSERT INTO users VALUES
 (1,'Kovács','András','kovacs.andras@email.hu','1990-05-12'),
 (2,'Nagy','Eszter','nagy.eszter@email.hu','1988-11-03'),
@@ -113,24 +117,26 @@ INSERT INTO users VALUES
 (7,'Molnár','Gábor','molnar.gabor@email.hu','1993-04-17'),
 (8,'Horváth','Zsófia','horvath.zsofia@email.hu','1991-06-25');
 
+-- Cars
 INSERT INTO cars VALUES
-(1,'Toyota','Corolla',1,2021,2,3,5,8990000),
-(2,'Volkswagen','Golf',2,2019,1,1,5,6590000),
-(3,'BMW','320d',4,2020,2,2,5,11290000),
-(4,'Ford','Focus',3,2018,1,1,5,5290000),
-(5,'Audi','A4',5,2022,2,2,5,13490000),
-(6,'Hyundai','Tucson',6,2021,2,3,5,10490000),
-(7,'Skoda','Octavia',7,2020,1,1,5,7890000),
-(8,'Suzuki','Vitara',8,2019,1,1,5,5990000),
-(9,'Mercedes','C220',1,2022,2,2,5,15990000),
-(10,'Kia','Sportage',4,2021,2,3,5,10990000),
-(11,'Opel','Astra',6,2019,1,1,5,5490000),
-(12,'Renault','Megane',3,2020,1,2,5,6390000),
-(13,'Tesla','Model 3',2,2023,2,4,5,17990000),
-(14,'Peugeot','3008',5,2021,2,2,5,9890000),
-(15,'Nissan','Qashqai',7,2022,2,1,5,11990000),
-(16,'Mazda','CX-5',8,2020,2,1,5,10490000);
+(1,'Toyota','Corolla',2021,5,8990000,'AB-CD-101',1,2,3),
+(2,'Volkswagen','Golf',2019,5,6590000,'EF-GH-214',2,1,1),
+(3,'BMW','320d',2020,5,11290000,'IJ-KL-335',4,2,2),
+(4,'Ford','Focus',2018,5,5290000,'MN-OP-442',3,1,1),
+(5,'Audi','A4',2022,5,13490000,'QR-ST-558',5,2,2),
+(6,'Hyundai','Tucson',2021,5,10490000,'UV-WX-671',6,2,3),
+(7,'Skoda','Octavia',2020,5,7890000,'YZ-AB-782',7,1,1),
+(8,'Suzuki','Vitara',2019,5,5990000,'CD-EF-893',8,1,1),
+(9,'Mercedes','C220',2022,5,15990000,'GH-IJ-904',1,2,2),
+(10,'Kia','Sportage',2021,5,10990000,'KL-MN-315',4,2,3),
+(11,'Opel','Astra',2019,5,5490000,'OP-QR-426',6,1,1),
+(12,'Renault','Megane',2020,5,6390000,'ST-UV-537',3,1,2),
+(13,'Tesla','Model 3',2023,5,17990000,'WX-YZ-648',2,2,4),
+(14,'Peugeot','3008',2021,5,9890000,'AA-BB-759',5,2,2),
+(15,'Nissan','Qashqai',2022,5,11990000,'CC-DD-860',7,2,1),
+(16,'Mazda','CX-5',2020,5,10490000,'EE-FF-971',8,2,1);
 
+-- Additional Equipment
 INSERT INTO additional_equipment VALUES
 (1,1,TRUE,2,TRUE,TRUE,FALSE),
 (2,2,FALSE,1,FALSE,TRUE,FALSE),
@@ -149,6 +155,7 @@ INSERT INTO additional_equipment VALUES
 (15,15,TRUE,2,TRUE,TRUE,FALSE),
 (16,16,TRUE,2,TRUE,TRUE,FALSE);
 
+-- Rentals
 INSERT INTO rentals VALUES
 (1,3,1,'2025-03-01','2025-03-05'),
 (2,8,2,'2025-03-02','2025-03-04'),
