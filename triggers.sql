460c46126d60df3f8f9e8fa32f20ce229221c795
kecske5000 <marton.kristof@students.jedlik.eu>
2026-03-21 12:32:32 +0100

    Triggers.sql updated

--- triggers.sql
+++ triggers.sql
@@ -9,31 +9,9 @@ BEGIN
     END IF;
 END;
 
--- 2. Új bérlés felvétele után: ha a bérlés kezdete ma van, az autó nem elérhető
-CREATE TRIGGER car_rental.after_rental_insert
-AFTER INSERT ON car_rental.rentals
-FOR EACH ROW
-BEGIN
-    IF CURDATE() BETWEEN NEW.start_date AND NEW.end_date THEN
-        UPDATE cars
-        SET availability = FALSE
-        WHERE id = NEW.car_id;
-    END IF;
-END;
 
--- 3. Bérlés frissítése után: ha visszahozta a bérlő az autót, az autó újra elérhető
-CREATE TRIGGER car_rental.after_rental_update
-AFTER UPDATE ON car_rental.rentals
-FOR EACH ROW
-BEGIN
-    IF NEW.return_date IS NOT NULL THEN
-        UPDATE cars
-        SET availability = TRUE
-        WHERE id = NEW.car_id;
-    END IF;
-END;
 
--- 4. Bérlés törlése előtt: csak akkor lehet törölni, ha már visszahozták az autót
+-- 2. Bérlés törlése előtt: csak akkor lehet törölni, ha már visszahozták az autót
 CREATE TRIGGER car_rental.before_rental_delete
 BEFORE DELETE ON car_rental.rentals
 FOR EACH ROW
