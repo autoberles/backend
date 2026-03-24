433cc69ae21c9d8dd04672dd1c59821b2777e1b9
kecske5000 <marton.kristof@students.jedlik.eu>
2026-03-24 08:41:55 +0100

    ER diagram updated

--- autoberles_backend/autoberles_backend/obj/Debug/net8.0/autoberles_backend.AssemblyInfo.cs
+++ autoberles_backend/autoberles_backend/obj/Debug/net8.0/autoberles_backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("autoberles_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+542c04e4f1397fa6d1d8169094ad485f04494f6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e765733823530dfeb22488556edc5515d51d653f")]
 [assembly: System.Reflection.AssemblyProductAttribute("autoberles_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("autoberles_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
